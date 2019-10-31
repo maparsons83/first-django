68ab10152c2e31fcd435620eabf15042c7c3d4be
David Stewart <davidstewy@gmail.com>
2019-10-31 19:49:40 -0400

    added logout/login view and login/logout html

--- Welcome/Welcome/views.py
+++ Welcome/Welcome/views.py
@@ -43,3 +43,28 @@ def recipes(request, author_id, recipe_name):
         }
     }
     return render(request, html, data_obj)
+
+def login_view(request):
+    html = 'login.html'
+
+    form = Login_Form(None or request.POST)
+
+    if form.is_valid():
+        next = request.POST.get('next')
+        data = form.cleaned_data
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is not None:
+            login(request, user)
+            # return HttpResponseRedirect(reverse('homepage'))
+        if next:
+            return HttpResponseRedirect(next)
+        else:
+            return HttpResponseRedirect('/')
+
+    return render(request, html, {'form':form})
+
+def logout_view(request):
+    html = 'logout.html'
+    logout(request)
+
+    return render(request, html)
