e44c73b4902ef5ef88aeb17eb807218319cfa3be
Matt Parsons <maparsons83@yahoo.com>
2019-10-30 21:18:06 -0400

    added first view

---
+++ Welcome/Welcome/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, reverse
+from django.contrib.auth.models import User
+
+from Welcome.models import Recipe
+from Welcome.models import Author
+
+def homepage(request):
+    html = 'recipes.html'
+
+    results = Recipe.objects.all()
+
+    return render(request, html, {'data': results})
